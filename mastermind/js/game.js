0d0ad37805cc7b1e81076af04c2f6855e1db359d
Robert Rajakone <robie2011@gmail.com>
2016-03-16 02:49:14 +0100

    mastermind: tipp kann abgebeben werden, hinweis wird korrekt angegeben

--- mastermind/js/game.js
+++ mastermind/js/game.js
@@ -9,17 +9,31 @@ var arrayReduceToSum = function(previousValue, currentValue) {
     return previousValue + currentValue;
 }
 
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+var getRandomArbitrary = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+var solutionArray = [];
+solutionArray.push(getRandomArbitrary(1,5));
+solutionArray.push(getRandomArbitrary(1,5));
+solutionArray.push(getRandomArbitrary(1,5));
+solutionArray.push(getRandomArbitrary(1,5));
+console.log("solution created: " + solutionArray);
+
 // Return a solution hint array containing: 1 = correct color or 2 = correct color+position
 var calculateSolutionHint = function (inputArray, solutionArray) {
     var solutionHintArray = [];
     
-    var solutionNumberCounter = [];
+    var solutionNumberCounter = [0, 0, 0, 0];
     solutionArray.forEach(function(value){
         if (!solutionNumberCounter[value]) solutionNumberCounter[value] = 1;
         else solutionNumberCounter[value] = ++solutionNumberCounter[value]; //POST INCREMENT DOESN'T WORK! WTF!
     });
     
-    var inputNumberCounter = [];    
+    var inputNumberCounter = [0, 0, 0, 0];    
     inputArray.forEach(function(value){
         if (!inputNumberCounter[value]) inputNumberCounter[value] = 1;
         else inputNumberCounter[value] = ++inputNumberCounter[value]; //POST INCREMENT DOESN'T WORK! WTF!
@@ -27,7 +41,7 @@ var calculateSolutionHint = function (inputArray, solutionArray) {
     
     var totalCorrectColorAndPositionInput = 0;
     solutionArray.forEach(function(value, index){
-       if (value === inputArray[index]) ++totalCorrectColorAndPositionInput;
+       if (value === Number(inputArray[index])) ++totalCorrectColorAndPositionInput;
     });
     
     var totalCorrectColorInputs = 0;
@@ -43,62 +57,11 @@ var calculateSolutionHint = function (inputArray, solutionArray) {
     for (var i = 0; i < totalCorrectColorAndPositionInput; i++)
         solutionHintArray.push(2);
     
+    console.log("input " + inputArray);
+    console.log("hint " + solutionHintArray);
     return solutionHintArray;
 }; 
 
-function getGamePlayHtml(inputArray, solutionArray) {
-// example html to generate
-//    <div class="game row">
-//    <div class="try col-lg-4 col-md-4 col-xs-5">
-//        <div style="background-color: red"></div>
-//        <div style="background-color: red"></div>
-//        <div style="background-color: red"></div>
-//        <div style="background-color: red"></div>
-//    </div>
-//    <div class="col-lg-1 col-md-1 col-xs-3">
-//        <div class="row">
-//            <div class="circle"></div>
-//            <div class="circle"></div>
-//        </div>
-//        <div class="row">
-//            <div class="circle"></div>
-//            <div class="circle"></div>
-//        </div>
-//    </div>
-//</div>
-    
-    var html = '';
-    html += '<div class="game row">\r\n';
-    
-    // entered colors
-    html += '<div class="try col-lg-4 col-md-4 col-xs-5">\r\n';
-    for (var i = 0; i < inputArray.length; i++)
-    {
-        html += '<div style="background-color: ' + colorMapping(inputArray[i]) +'"/>\r\n';
-    }    
-    html += '</div>\r\n';
-    // -----------------------------------------------------------
-    
-    // Solution Hint Color Coding
-    html += '<div class="col-lg-1 col-md-1 col-xs-3">\r\n';
-    html += '<div class="row">';
-    for (var i=0; i < solutionHintArray.length; i++)
-    {
-        var thereAreMoreThanTwoHints = i % 2 == 0;
-        if (thereAreMoreThanTwoHints)
-        {
-            html += '<div class="row">';
-            html += '</div>\r\n';            
-        }
-    }
-    html += '</div>\r\n';
-    html += '</div>\r\n';
-    // -----------------------------------------------------------
-    
-    html += '</div>\r\n';
-    
-    return html;
-}
 
 //console.log(calculateSolutionHint([1,1,1,1], [1,2,3,4]));
 //console.log(calculateSolutionHint([1,2,3,4], [1,2,3,4]));
@@ -112,13 +75,19 @@ var canSubmit = function(){
         && $('#input4').attr('data-code');
 };
 
-var addGamePlay = function (inputArray){
+var addGamePlay = function (inputArray, solutionHintArray){
     var clonedTryTpl = $('#templates > .try').clone();    
     var colorBoxes = $(clonedTryTpl).find('.colorBox');
     colorBoxes[0] = $(colorBoxes[0]).attr('data-code', inputArray[0]);
     colorBoxes[1] = $(colorBoxes[1]).attr('data-code', inputArray[1]);
     colorBoxes[2] = $(colorBoxes[2]).attr('data-code', inputArray[2]);
     colorBoxes[3] = $(colorBoxes[3]).attr('data-code', inputArray[3]);
+    
+    var solutionHintBoxes = $(clonedTryTpl).find('.circle');
+    solutionHintBoxes[0] = $(solutionHintBoxes[0]).attr('data-hint-code', solutionHintArray[0]);
+    solutionHintBoxes[1] = $(solutionHintBoxes[1]).attr('data-hint-code', solutionHintArray[1]);
+    solutionHintBoxes[2] = $(solutionHintBoxes[2]).attr('data-hint-code', solutionHintArray[2]);
+    solutionHintBoxes[3] = $(solutionHintBoxes[3]).attr('data-hint-code', solutionHintArray[3]);    
     $(clonedTryTpl).appendTo('#game');
 };
 
@@ -149,6 +118,7 @@ function play(){
     inputArray.push($('#input3').attr('data-code'));
     inputArray.push($('#input4').attr('data-code'));
     
-    addGamePlay(inputArray)
+    var solutionHintArray = calculateSolutionHint(inputArray, solutionArray);    
+    addGamePlay(inputArray, solutionHintArray);
     resetUi();
 }
