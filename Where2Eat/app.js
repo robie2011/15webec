c840a69a171af7989a6488225ae02ac45431bafe
Robert Rajakone <robie2011@gmail.com>
2016-04-16 19:41:01 +0200

    where2eat: impl what+who

--- Where2Eat/app.js
+++ Where2Eat/app.js
@@ -10,13 +10,13 @@ var where2Eat = (function () {
         var _this = this;
         this.position = { lat: 0, lng: 0 };
         this.apiKey = "AIzaSyBVJCxnnxByXHof9D-Dw_brILrxQVgF9Ik";
-        this.googleMap = null;
+        this.whoResults = [];
         this.setPosition = function (p) {
             _this.position = { lat: p.coords.latitude, lng: p.coords.longitude };
-            console.log("Position set: lat/long");
-            console.log(_this.position);
             _this.googleMap.setCenter(_this.position);
             _this.googleMap.setZoom(13);
+            var radiusInMeter = 1000;
+            _this.searchForPlaceNearby('', radiusInMeter);
         };
         this.refreshGeoposition = function () {
             if (navigator.geolocation) {
@@ -24,48 +24,115 @@ var where2Eat = (function () {
             }
         };
         this.setGoogleMap = function (m) { return _this.googleMap = m; };
-        this.searchForPlaceNearby = function (places, radius) {
+        this.searchForPlaceNearby = function (keyword, radius) {
             infowindow = new google.maps.InfoWindow();
             var placeService = new google.maps.places.PlacesService(_this.googleMap);
             placeService.nearbySearch({
                 location: _this.position,
                 radius: radius,
-                type: places
+                type: ['restaurant'],
+                keyword: keyword
             }, _this.placeServiceCallback);
         };
         this.placeServiceCallback = function (results, status) {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
                 console.log("found total: " + results.length);
-                console.log(results);
+                _this.whoResults = results;
+                _this.clearMarkers();
                 for (var i = 0; i < results.length; i++) {
-                    _this.createMarker(results[i]);
+                    _this.createMarker(results[i], i + 1);
                 }
             }
         };
-        this.createMarker = function (place) {
+        this.markers = [];
+        this.clearMarkers = function () {
+            if (_this.markers.length > 0) {
+                for (var i = 0; i < _this.markers.length; i++) {
+                    _this.markers[i].setMap(null);
+                }
+            }
+        };
+        this.createMarker = function (place, number) {
             var placeLoc = place.geometry.location;
             var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
+                map: _this.googleMap,
+                position: place.geometry.location,
+                icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + number + '|FF0000|000000'
             });
+            _this.markers.push(marker);
             google.maps.event.addListener(marker, 'click', function () {
                 infowindow.setContent(place.name);
-                infowindow.open(map, this);
+                infowindow.open(this.map, this);
             });
         };
+        this.refreshPosition = function () {
+            _this.refreshGeoposition();
+        };
+        this.initMap = function () {
+            _this.googleMap = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 0, lng: 0 },
+                zoom: 8,
+                streetViewControl: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                mapTypeControl: false
+            });
+        };
+        this.setFoodPreference = function (what) {
+            var radiusInMeter = 1000;
+            _this.searchForPlaceNearby(what, radiusInMeter);
+        };
+        this.getWhos = function () {
+            return _this.whoResults;
+        };
         this.refreshGeoposition();
+        this.initMap();
     }
     return where2Eat;
 })();
 var app = new where2Eat();
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 0, lng: 0 },
-        zoom: 8
+var hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront = function (elementId) {
+    if ($(elementId).css('display') == 'block') {
+        $(elementId).css('display', 'none');
+        $('#map').css('z-index', '');
+        return true;
+    }
+    $(elementId).css('display', 'block');
+    $('#map').css('z-index', -1);
+    return false;
+};
+var showWhat = function () {
+    if (hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront('#what')) {
+        return;
+    }
+    hideWho();
+};
+var hideWhat = function () {
+    $('#map').css('z-index', '');
+    $('#what').css('display', 'none');
+};
+var hideWho = function () {
+    $('#who').css('display', 'none');
+};
+var initWhatCallbackHandlers = function () {
+    $('#what td').click(function () {
+        app.setFoodPreference($(this).context.innerText);
+        hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront('#what');
     });
-    app.setGoogleMap(map);
-}
-initMap();
-//# sourceMappingURL=app.js.map 
+};
+var showWhoResults = function () {
+    if (hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront('#who')) {
+        return;
+    }
+    hideWhat();
+    var results = app.getWhos();
+    var resultList = $('#who>table');
+    resultList.empty();
+    for (var i = 0; i < results.length; i++) {
+        var r = "<h3>" + (i + 1) + " " + results[i].name + " </h3>";
+        r += "<p>" + results[i].vicinity + "</p>";
+        r = '<tr><td>' + r + '</td></tr>';
+        $(r).appendTo(resultList);
+    }
+};
+initWhatCallbackHandlers();
 //# sourceMappingURL=app.js.map
--- Where2Eat/app.ts
+++ Where2Eat/app.ts
@@ -3,68 +3,165 @@
  * Place Tutorial: https://developers.google.com/maps/documentation/javascript/examples/place-search
  * TODO: Markers removing https://developers.google.com/maps/documentation/javascript/examples/marker-remove?hl=de
  */
-//todo
+
+    //todo
 var infowindow;
-var where2Eat = (function () {
-    function where2Eat() {
-        var _this = this;
-        this.position = { lat: 0, lng: 0 };
-        this.apiKey = "AIzaSyBVJCxnnxByXHof9D-Dw_brILrxQVgF9Ik";
-        this.googleMap = null;
-        this.setPosition = function (p) {
-            _this.position = { lat: p.coords.latitude, lng: p.coords.longitude };
-            console.log("Position set: lat/long");
-            console.log(_this.position);
-            _this.googleMap.setCenter(_this.position);
-            _this.googleMap.setZoom(13);
-        };
-        this.refreshGeoposition = function () {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(_this.setPosition);
+
+class where2Eat {
+    private position = { lat: 0, lng: 0};
+    private apiKey = "AIzaSyBVJCxnnxByXHof9D-Dw_brILrxQVgF9Ik";
+    private googleMap;
+    private whoResults: Object[] = [];
+
+
+    private setPosition: PositionCallback = p => {
+        this.position = { lat: p.coords.latitude, lng: p.coords.longitude };
+        this.googleMap.setCenter(this.position);
+        this.googleMap.setZoom(13);
+
+        var radiusInMeter = 1000;        
+        this.searchForPlaceNearby('', radiusInMeter);
+    }
+
+    private refreshGeoposition = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.setPosition);
+        }
+    }
+
+    public setGoogleMap = m => this.googleMap = m;
+
+    public searchForPlaceNearby = (keyword: string, radius: number){
+        infowindow = new google.maps.InfoWindow();
+        var placeService = new google.maps.places.PlacesService(this.googleMap);
+        placeService.nearbySearch({
+            location: this.position,
+            radius: radius,
+            type: ['restaurant'],
+            keyword: keyword
+        }, this.placeServiceCallback);
+    }
+
+    private placeServiceCallback = (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {     
+            console.log("found total: " + results.length);
+            this.whoResults = results;
+            this.clearMarkers();
+            
+            for (var i = 0; i < results.length; i++) {
+                this.createMarker(results[i], i+1);
             }
-        };
-        this.setGoogleMap = function (m) { return _this.googleMap = m; };
-        this.searchForPlaceNearby = function (places, radius) {
-            infowindow = new google.maps.InfoWindow();
-            var placeService = new google.maps.places.PlacesService(_this.googleMap);
-            placeService.nearbySearch({
-                location: _this.position,
-                radius: radius,
-                type: places
-            }, _this.placeServiceCallback);
-        };
-        this.placeServiceCallback = function (results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                console.log("found total: " + results.length);
-                console.log(results);
-                for (var i = 0; i < results.length; i++) {
-                    _this.createMarker(results[i]);
-                }
+        }
+    }
+
+    private markers: Object[] = [];
+    private clearMarkers = () => {
+        if (this.markers.length > 0) {
+            for (var i = 0; i < this.markers.length; i++) {
+                this.markers[i].setMap(null);
             }
-        };
-        this.createMarker = function (place) {
-            var placeLoc = place.geometry.location;
-            var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
-            });
-            google.maps.event.addListener(marker, 'click', function () {
-                infowindow.setContent(place.name);
-                infowindow.open(map, this);
-            });
-        };
+        }
+    }
+    private createMarker = (place, number) => {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: place.geometry.location,
+            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld='+ number + '|FF0000|000000'
+        });
+
+        this.markers.push(marker);
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.setContent(place.name);
+            infowindow.open(this.map, this);
+        });
+    }
+
+    public refreshPosition = () => {
         this.refreshGeoposition();
     }
-    return where2Eat;
-})();
-var app = new where2Eat();
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+
+
+    private initMap = () => {
+        this.googleMap = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 0, lng: 0 },
-        zoom: 8
-    });
-    app.setGoogleMap(map);
+        zoom: 8,
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false
+        });
+    }
+
+    public setFoodPreference = (what: string) => {
+        var radiusInMeter = 1000;
+        this.searchForPlaceNearby(what, radiusInMeter);
+    }
+
+    public getWhos = () => {
+        return this.whoResults;
+    }
+
+    constructor() {
+        this.refreshGeoposition();
+        this.initMap();
+    }    
 }
-initMap();
-//# sourceMappingURL=app.js.map
+
+var app = new where2Eat();
+
+var hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront = (elementId: string) => {
+    if ($(elementId).css('display') == 'block') {
+        $(elementId).css('display', 'none');
+        $('#map').css('z-index', '');
+        return true;
+    }
+    $(elementId).css('display', 'block');
+    $('#map').css('z-index', -1);
+    return false;
+}
+
+var showWhat = () => {
+    if (hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront('#what')) {
+        return;
+    }
+    hideWho();
+};
+
+var hideWhat = () => {
+    $('#map').css('z-index', '');
+    $('#what').css('display', 'none');
+}
+
+var hideWho = () => {
+    $('#who').css('display', 'none');
+}
+
+var initWhatCallbackHandlers = () => {
+    $('#what td').click(function () {
+        app.setFoodPreference($(this).context.innerText);
+        hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront('#what');
+    });
+}
+
+var showWhoResults = () => {
+    if (hideMenuIfActiveAndActiveMapOtherwiseBrigItToTheFront('#who')) {
+        return;
+    }
+
+    hideWhat();
+    var results = app.getWhos();
+    var resultList = $('#who>table');
+    resultList.empty();
+     
+    for (var i = 0; i < results.length; i++) {
+        var r = "<h3>" + (i + 1) + " " + results[i].name + " </h3>";
+        r += "<p>" + results[i].vicinity + "</p>";
+        r = '<tr><td>' + r + '</td></tr>';
+        $(r).appendTo(resultList);
+    }
+}
+
+
+initWhatCallbackHandlers();
+
